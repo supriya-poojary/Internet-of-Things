ebdc709a99bfa2cb9dc3c872b5e33ae4aa27f9a3
supriya-poojary <153600638+supriya-poojary@users.noreply.github.com>
2025-10-05 19:58:20 +0530

    Create Lab2.ino

---
+++ Lab2.ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(8, INPUT);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int temp=digitalRead(8);
+  Serial.println("Intensity");
+  Serial.println(temp);
+  delay(300);
+  if(temp==HIGH)
+  digitalWrite(9, HIGH);
+  else
+  digitalWrite(9, LOW);
+}
